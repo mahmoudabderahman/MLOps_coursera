5b49a42b1ea883948092e0da39a917e015ac617e
MahmoudAbderahman <mahmoud.abou-auf@outlook.com>
2021-10-01 01:44:04 +0200

    Some fixes

--- week1/src/server.py
+++ week1/src/server.py
@@ -21,21 +21,23 @@ if not os.path.exists(dir_name):
 # You will interact with your api using this instance.
 app = FastAPI(title='Deploying a ML Model with FastAPI')
 
+
 # List available models using Enum for convenience. This is useful when the options are pre-defined.
 class Model(str, Enum):
     yolov3tiny = "yolov3-tiny"
     yolov3 = "yolov3"
+
 
 # By using @app.get("/") you are allowing the GET method to work for the / endpoint.
 @app.get("/")
 def home():
     return "Congratulations! Your API is working as expected. Now head over to http://localhost:8000/docs."
 
+
 # This endpoint handles all the logic necessary for the object detection to work.
 # It requires the desired model and the image in which to perform object detection.
 @app.post("/predict")
 def prediction(model: Model, file: UploadFile = File(...)):
-
     # 1. VALIDATE INPUT FILE
     filename = file.filename
     fileExtension = filename.split(".")[-1] in ("jpg", "jpeg", "png")
@@ -65,22 +67,22 @@ def prediction(model: Model, file: UploadFile = File(...)):
     output_image = draw_bbox(image, bbox, label, conf)
 
     # Save it in a folder within the server
-    cv2.imwrite(f'images_uploaded/{filename}', output_image)
+    cv2.imwrite(f'../images_uploaded/{filename}', output_image)
 
     # 4. STREAM THE RESPONSE BACK TO THE CLIENT
 
     # Open the saved image for reading in binary mode
-    file_image = open(f'images_uploaded/{filename}', mode="rb")
+    file_image = open(f'../images_uploaded/{filename}', mode="rb")
 
     # Return the image as a stream specifying media type
     return StreamingResponse(file_image, media_type="image/jpeg")
 
+
 # Allows the server to be run in the interactive environment
-#nest_asyncio.apply()
+# nest_asyncio.apply()
 
 # Host depends on the setup you selected (docker or virtual env)
 host = "0.0.0.0" if os.getenv("DOCKER-SETUP") else "127.0.0.1"
 
 # Spin up the server!
-uvicorn.run(app,host=host, port=8000)
-
+uvicorn.run(app, host=host, port=8000)
